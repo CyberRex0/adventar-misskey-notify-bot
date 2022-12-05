aa155a3ca9bfb1de6e072952576c5b1642bc2d6e
CyberRex0 <hspwinx86@gmail.com>
2022-12-06 01:30:01 +0900

    Improve code, Fix hardcoding, add Dry-Run Mode

--- adventar.py
+++ adventar.py
@@ -6,6 +6,11 @@ import time
 import config
 from misskey import Misskey
 import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dry-run', action='store_true')
+args = parser.parse_args()
 
 EOL = '\n'
 
@@ -48,6 +53,7 @@ for dayEl, itemEl in zip(entries_day, entries):
     t = itemEl.text
     title = None
     url = None
+    articleTitle = None
     if len(t.split('\n')) >= 2:
         _x = t.split('\n')
         title = _x[1]
@@ -56,11 +62,21 @@ for dayEl, itemEl in zip(entries_day, entries):
         url = linkEl.get_attribute('href')
     except:
         pass
+    try:
+        articleTitleEl = itemEl.find_element(By.XPATH, ".//div[@class='article']/div[@class='left']/div[not(@class='link')]")
+        articleTitle = articleTitleEl.text.split('|', 1)[0]
+    except:
+        pass
+    calendar[date]['articleTitle'] = articleTitle
     calendar[date]['title'] = title
     calendar[date]['url'] = url
 
 for date, info in calendar.items():
     print(f'12/{date}: {info["user"]} - {info["title"]} ({info["url"]})')
+
+# get caledar title
+calTitleEl = driver.find_element(By.XPATH, '//header[@class="header"]/div[@class="inner"]/h2[@class="title"]')
+calTitle = calTitleEl.text
 
 driver.quit()
 
@@ -72,7 +88,13 @@ if today.month == 12:
                 entry_title = calendar[today.day]['title']
                 entry_url = calendar[today.day]['url']
                 entry_user = calendar[today.day]['user']
-                mi.notes_create(**{
-                    'text': f'Misskey Advent Calendar {today.year}\nDay {today.day} 「{entry_title or "(タイトル未設定)"}」 by {entry_user}\n{entry_url or "(URL未設定)"+EOL+CALENDAR_URL}',
-                    'visibility': config.NOTE_VISIBILITY
-                })
+                entry_article_title = calendar[today.day]['articleTitle']
+                text = f'{calTitle}{" "+str(today.year) if config.SHOW_YEAR else ""}\nDay {today.day} 「{entry_title or "(タイトル未設定)"}」 by {entry_user}\n\n{entry_article_title+EOL or ""}{entry_url or "(URL未設定)"+EOL+CALENDAR_URL}'
+                if not args.dry_run:
+                    mi.notes_create(**{
+                        'text': text,
+                        'visibility': config.NOTE_VISIBILITY
+                    })
+                else:
+                    print('---DRY RUN---')
+                    print(text)
