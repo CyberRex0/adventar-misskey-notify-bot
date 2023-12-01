31d8371b1a5149ab18fa2a1dd8807f76db4f8458
CyberRex <hspwinx86@gmail.com>
2023-12-01 18:27:26 +0900

    Update .gitignore

--- config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = 'YourMisskeyTokenHere'
-DOMAIN = 'YourMisskeyInstanceDomainHere' # e.g. misskey.io
-CALENDAR_ID = 12345 # calendar ID here (https://adventar.org/calendars/12345 -> 12345)
-NOTE_VISIBILITY = 'public' # public, home, followers
-SHOW_YEAR = False # whether to show the year in the end of calendar title (e.g. ABC Advent Calendar 2020)
